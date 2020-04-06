8c788e0493ad0fb3a546a9e5b6f4cebc696140e0
Heisenberg <Persey_98@list.ru>
2020-04-06 15:29:35 +0500

    created constructor Cats

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Homework6_3
     {
         public string Name { get; set; }
         public string Color { get; set; }
-        public string EyesColor;
+        public string EyesColor { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Cats(string Name, string Color, string EyesColor, int Age, string Gender)
+        {
+            this.Name = Name;
+            this.Color = Color;
+            this.EyesColor = EyesColor;
+            this.Age = Age;
+            this.Gender = Gender;
+        }
     }
 
 
