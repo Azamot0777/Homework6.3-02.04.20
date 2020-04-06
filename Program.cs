93c4107770a197488c32d54b736cb72080c725e1
Heisenberg <Persey_98@list.ru>
2020-04-06 15:22:50 +0500

    created class cats

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework6_3
+{
+    class Cats
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string EyesColor;
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
