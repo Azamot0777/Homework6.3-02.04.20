ba33fb493f868b5cfa7b9d9db51b9fe8531c5381
Heisenberg <Persey_98@list.ru>
2020-04-06 16:58:21 +0500

    Вызвал классы в Main

--- Program.cs
+++ Program.cs
@@ -10,32 +10,34 @@ namespace Homework6_3
         public int Age { get; set; }
         public string Gender { get; set; }
 
-        public Cats(string Name, string Color, string EyesColor, int Age, string Gender)
+        public Cats(string name, string color, string eyescolor, int age, string gender)
         {
-            this.Name = Name;
-            this.Color = Color;
-            this.EyesColor = EyesColor;
-            this.Age = Age;
-            this.Gender = Gender;
+            this.Name = name;
+            this.Color = color;
+            this.EyesColor = eyescolor;
+            this.Age = age;
+            this.Gender = gender;
         }
         public void GetInfo()
         {
+            Console.WriteLine("///////////////////");
             Console.WriteLine($"Cats name :{this.Name}");
             Console.WriteLine($"Cats color :{this.Color}");
             Console.WriteLine($"Cats Eyes color :{this.EyesColor}");
-            Console.WriteLine($"Cats age :{this.Age}");
+            Console.WriteLine($"Cats age :{this.Age} month");
             Console.WriteLine($"Cats gender :{this.Gender}");
         }
     }
-
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-
+            Cats k1 = new Cats("Чернышь", "Черный", "Зеленые", 2, "Мальчик");
+            Cats k2 = new Cats("Кайковус", "Белый", "Голубые", 5, "Девочка");
+            Cats k3 = new Cats("Бульон", "Мокрый асфальт", "Желтые", 9, "Мальчик");
+            k1.GetInfo();
+            k2.GetInfo();
+            k3.GetInfo();
         }
     }
 }
